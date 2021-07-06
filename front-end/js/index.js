c0d6b1a9c59e79750065baaeadb3e82ae61ad4c0
Maïmouna <maimouna.koehlerbonnabel@gmail.com>
2021-07-06 17:36:11 +0200

    Début JavaSacript

--- front-end/js/index.js
+++ front-end/js/index.js
@@ -1,2 +1,12 @@
-let url = ""
-fetch(URL)
+let url = "http://localhost:3000/api/teddies"
+fetch(url)
+.then(data => {
+    return data.json()
+}).then(products => {
+    console.log(products)
+
+    products.forEach(product => {
+        console.log(product)
+        console.log(product.name)
+    });
+})
