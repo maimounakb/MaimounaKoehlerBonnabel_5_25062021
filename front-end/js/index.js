30a3adb21874f8fdfbea9b6fa1ec04c914fb316f
Maïmouna <maimouna.koehlerbonnabel@gmail.com>
2021-07-21 20:07:05 +0200

    Ajout du choix des couleurs

--- front-end/js/index.js
+++ front-end/js/index.js
@@ -1,33 +1,40 @@
-let url = "http://localhost:3000/api/teddies"
-fetch(url , {method : "GET"})
-.then(data => {
-    return data.json()
-}).then(products => {
-    console.log(products)
+let url = "http://localhost:3000/api/teddies";
+fetch(url, { method: "GET" })
+  .then((data) => {
+    return data.json();
+  })
+  .then((products) => {
+    console.log(products);
+
+    let elementIndex = document.getElementById("presentation-produits");
+    let urlPageProduit = "pages/produit.html";
+
+    let newHtmlIndex = "";
 
 
-    let elementIndex = document.getElementById("presentation-produits")
-    let urlPageProduit = 'pages/produit.html'
 
-    let newHtmlIndex = ""
-    products.forEach(product => {
-        /*console.log(product.name)
+    products.forEach((product) => {
+      /*console.log(product.name)
         console.log(product.price)
         console.log(product._id)*/
-        
-        newHtmlIndex += `<article class="produit">
-                        <a href="${urlPageProduit + "?_id=" + product._id}">
+
+      let price = new Intl.NumberFormat("fr-FR", {
+        style: "currency",
+        currency: "EUR",
+        minimumFractionDigits: 2,
+      }).format(product.price / 100);
+
+      newHtmlIndex += `<article class="produit">
+                        <a href="${urlPageProduit}?_id=${product._id}">
                             <img class="produit__image" src="${product.imageUrl}" alt="${product.name}">
                             <ul class="produit__description">
                                 <li><h3 class="">${product.name}</h3></li>
-                                <li><p class="">${product.price}</p></li>
+                                <li><p class="">${price}</p></li>
                             </ul>
                         </a>
-                    </article>`
+                    </article>`;
     });
 
     /*console.log(newHtmlIndex)*/
-    elementIndex.innerHTML = newHtmlIndex
-
-    
-})
+    elementIndex.innerHTML = newHtmlIndex;
+  });
--- front-end/js/produit.js
+++ front-end/js/produit.js
@@ -1,7 +1,7 @@
 /*console.log(window)*/
 var URLSearchParams = new URLSearchParams(window.location.search);
 var urlId = URLSearchParams.get("_id");
-console.log(urlId);
+/*console.log(urlId);*/
 
 
 let url = `http://localhost:3000/api/teddies/${urlId}`;
@@ -9,29 +9,54 @@ fetch(url , {method : "GET"})
 .then(data => {
     return data.json()
 }).then(product => {
-    console.log(product)
+    /*console.log(product)*/
    
-    let elementProduct = document.getElementById("produit")
+    let price = new Intl.NumberFormat("fr-FR", {
+        style: "currency",
+        currency: "EUR",
+        minimumFractionDigits: 2,
+      }).format(product.price / 100);
+
+    let elementProduct = document.getElementById("produit");
     /*console.log(elementProduct)*/
 
-    elementProduct.innerHTML = elementProduct += `<img class="presentation-produit__image" src="${product.imageUrl}" alt="${product.name}">
+    elementProduct.innerHTML =  `<img class="presentation-produit__image" src="${product.imageUrl}" alt="${product.name}">
                                                 <div class="presentation-produit__texte produit-texte">
                                                     <h2 class="produit-texte__titre">${product.name}</h2>
-                                                    <p class="produit-texte__prix">${product.price}</p>
+                                                    <p class="produit-texte__prix">${price}</p>
                                                     <div class="produit-texte__personnaliser personnaliser">
                                                         <form method="post" action="">
                                                             <p>
                                                                 <label for="couleur">Choisir la couleur :</label><br />
                                                                 <select name="couleur" id="couleur">
-                                                                    <option value=""></option>
-                                                                    <option value=""></option>
-                                                                    <option value=""></option>
                                                                 </select>
                                                             </p>
                                                         </form>
                                                     </div>
                                                     <p class="produit-texte__description">${product.description}</p>
                                                     <button class="produit-texte__bouton">Ajouter au panier</button>
-                                                </div>`
+                                                </div>`;
+
+
+    /*console.log(product.colors)*/
+
+    const choixCouleur = product.colors;
+
+    choixCouleur.forEach(color => {
+
+        const nouvelleCouleur = document.createElement("option");
+        let option = document.getElementById("couleur");
+        option.appendChild(nouvelleCouleur);
+        nouvelleCouleur.value = couleur;
+        nouvelleCouleur.textContent = color;
+        /*console.log(color)*/
+
+    });
+
+    /*
+    1. Récupérer donnée de couleur
+    2. Créer nouvelle balise pour chaque nouvelle couleur
+    3. Placer la balise (par rapport à son élément parent)
+    */
 
 });
